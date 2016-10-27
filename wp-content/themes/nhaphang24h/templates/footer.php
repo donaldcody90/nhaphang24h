4d1ef1286974cf7f430e465faf96f5b0730b43c3
Hoa Quan Van <hong7kong90@gmail.com>
2016-10-27 08:59:41 +0700

    message

--- wp-content/themes/nhaphang24h/templates/footer.php
+++ wp-content/themes/nhaphang24h/templates/footer.php
@@ -15,15 +15,7 @@
 							  <span class="address-title">Hà Nội</span>
 							  <ul class="address-list">
 								<li>
-								  <span class="address">Số 12 - ngõ 171 - Minh Khai - Hai Bà Trưng</span>
-								</li>
-								<li>
-								  <span class="address">P202-Tầng 2-Green Building- 134 Hoàng Sâm-Cầu Giấy</span>
-								</li>
-								<li>
-								  <span class="address">Số 5 - Ngõ 282 - Nguyễn Huy Tưởng - Quận Thanh Xuân</span>
-								</li>
-								<li>
+								  <span class="address">Số 139 - Thiên Hiền - Từ Liêm</span>
 								</li>
 							  </ul>
 							</div>
@@ -31,7 +23,7 @@
 							  <span class="address-title">Hồ Chí Minh</span>
 							  <ul class="address-list">
 								<li>
-								  <span class="address">Lầu 1, Số 92 - Đường số 2 - Cư Xá Đô Thành (Vườn Chuối) - P4 - Q3</span>
+								  <span class="address">Số 139 - Thiên Hiền - Từ Liêm</span>
 								</li>
 							  </ul>
 							</div>
@@ -50,16 +42,16 @@
 						  <span class="footer-block-title">Blog</span>
 						  <div class="footer-block-content">
 							<div class="block-post">
-							  <span class="post-title"><a href="/p97/nhung-san-pham-nhap-tu-han-quoc-duoc-ua-chuong-nhat">Những sản phẩm nhập từ Hàn Quốc được ưa chuộng nhất</a></span>
+							  <span class="post-title"><a href="#">Những sản phẩm nhập từ Hàn Quốc được ưa chuộng nhất</a></span>
 							</div>
 							<div class="block-post">
-							  <span class="post-title"><a href="/p87/loi-ich-khi-dat-mua-hang-trung-quoc-tren-taobao-tmall-coupang">Lợi ích khi đặt mua hàng trung quốc trên TAOBAO, TMALL, COUPANG </a></span>
+							  <span class="post-title"><a href="#">Lợi ích khi đặt mua hàng trung quốc trên TAOBAO, TMALL, COUPANG </a></span>
 							</div>
 							<div class="block-post">
-							  <span class="post-title"><a href="/p86/hang-quang-chau-lua-chon-hang-dau-cho-shop-online">Hàng Quảng Châu - Lựa chọn hàng đầu cho shop online</a></span>
+							  <span class="post-title"><a href="#">Hàng Quảng Châu - Lựa chọn hàng đầu cho shop online</a></span>
 							</div>
 							<div class="block-post">
-							  <span class="post-title"><a href="/p85/luu-y-khi-dat-hang-tren-taobaocom-1688com">Lưu ý khi đặt hàng trên taobao.com, 1688.com.</a></span>
+							  <span class="post-title"><a href="#">Lưu ý khi đặt hàng trên taobao.com, 1688.com.</a></span>
 							</div>
 						  </div>
 						</div>
@@ -67,34 +59,29 @@
 						  <span class="footer-block-title">Được chứng nhận</span>
 						  <div class="footer-block-content">
 							<div class="block-cert">
-							  <a href="http://merchant_cert.lab.baokim.vn/chung_nhan/00000/615_NHAPHANG24h_COM" target="_blank"><img src="/images/cert_baokim.png"></a>
+							  <a href="#" target="_blank"><img src="<?php echo get_template_directory_uri();?>/assets/img/cert_baokim.png"></a>
 							</div>
 							<div class="block-cert">
-							  <a href="http://online.gov.vn/CustomWebsiteDisplay.aspx?DocId=25249" target="_blank">
-							  <img src="/images/dathongbao.png" style="width: 138px; height: 52px;">
+							  <a href="#" target="_blank">
+							  <img src="<?php echo get_template_directory_uri();?>/assets/img/dathongbao.png" style="width: 138px; height: 52px;">
 							  </a>
 							</div>
 						  </div>
 						</div>
 						<span class="footer-line"></span>
 						<span class="footer-logo-2"></span>
-						<h1 class="company-name">công ty tnhh thương mại &amp; xnk việt com</h1>
-						<span style="float: left; clear: both; font-size: 13px; font-weight: 500; color: #565656; margin-top: -7px;">Mã số doanh nghiệp: 0106742467</span>
-						<span style="float: left; clear: both; font-size: 13px; font-weight: 500; color: #565656;">Do Sở KH và ĐT TP. Hà Nội cấp ngày 09/01/2015, cấp đổi lần 2 ngày 10/05/2016.</span>
-						<span style="float: left; clear: both; font-size: 13px; font-weight: 500; color: #565656;">Trụ sở chính: Số 12, ngõ 171 Minh Khai, Hai Bà Trưng, Hà Nội.</span>
+						<div class="footer-company-name">
+							<h1 class="company-name">công ty tnhh thương mại &amp; Chivi</h1>
+							<span>Mã số doanh nghiệp: 121212121</span>
+							<span>Do Sở KH và ĐT TP. Hà Nội cấp ngày 09/01/2015, cấp đổi lần 2 ngày 10/05/2016.</span>
+							<span>Trụ sở chính: Số 139 - Thiên Hiền - Từ Liêm.</span>
+						</div>
 						<div class="footer-info" style="margin-top: -5px;">
 						  <ul class="footer-link">
 							<li><a href="http://nhaphang24h.com/lien-he">Liên hệ</a></li>
 							<li><a href="http://nhaphang24h.com/huong-dan-dat-hang">Hướng dẫn đặt hàng</a></li>
 							<li><a href="http://nhaphang24h.com/bang-gia">Bảng giá</a></li>
 							<li><a href="http://nhaphang24h.com/chinh-sach-bao-mat">Chính sách bảo mật</a></li>
-						  </ul>
-						  <ul class="footer-social-icon">
-							<li><a class="facebook" href="https://www.facebook.com/ordertaobao68" target="_blank"></a></li>
-							<li><a class="linkedin" href="javascript:;"></a></li>
-							<li><a class="twitter" href="#"></a></li>
-							<li><a class="gplus" href="#"></a></li>
-							<li><a class="youtube" href="#"></a></li>
 						  </ul>
 						</div>
 					  </div>
